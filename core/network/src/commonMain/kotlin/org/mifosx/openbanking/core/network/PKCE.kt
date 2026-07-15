1b585c9306569d21e930dbae5669ef6f2ee77c58
thekalpeshpawar <pawarkalpesh@proton.me>
2026-07-15 13:29:39 +0530

    spotless and detekt licence changes.
    Signed-off-by: thekalpeshpawar <pawarkalpesh@proton.me>

--- core/network/src/commonMain/kotlin/org/mifosx/openbanking/core/network/PKCE.kt
+++ core/network/src/commonMain/kotlin/org/mifosx/openbanking/core/network/PKCE.kt
@@ -5,7 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * See See https://github.com/openMF/kmp-project-template/blob/main/LICENSE
+ * See See https://github.com/openMF/mifos-x-open-banking/blob/dev/LICENSE
  */
 package org.mifosx.openbanking.core.network
 
